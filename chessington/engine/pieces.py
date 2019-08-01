adcf6deced2c2d05cbafbe1663f713f914db8b16
Sam Cappleman-Lynes <sam.cappleman-lynes@softwire.com>
2019-08-01 14:28:36 +0100

    Pawn border enforcement

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -38,26 +38,30 @@ class Pawn(Piece):
     def get_available_moves(self, board):
         current_square = board.find_piece(self)
         if self.player == Player.WHITE:
-            if board.is_square_empty(Square(current_square.row + 1, current_square.col)):
-                next_square = Square.at(current_square.row + 1, current_square.col)
-                if current_square.row == 1:
-                    if board.is_square_empty(Square(current_square.row + 2, current_square.col)):
-                        next_square_two_step = Square.at(current_square.row + 2, current_square.col)
-                        return [next_square, next_square_two_step]
+            if current_square.row != 7:
+                if board.is_square_empty(Square(current_square.row + 1, current_square.col)):
+                    next_square = Square.at(current_square.row + 1, current_square.col)
+                    if current_square.row == 1:
+                        if board.is_square_empty(Square(current_square.row + 2, current_square.col)):
+                            next_square_two_step = Square.at(current_square.row + 2, current_square.col)
+                            return [next_square, next_square_two_step]
+                        return [next_square]
                     return [next_square]
-                return [next_square]
-            return []
+                return []
+            return[]
 
         if self.player == Player.BLACK:
-            if board.is_square_empty(Square(current_square.row - 1, current_square.col)):
-                next_square = Square.at(current_square.row - 1, current_square.col)
-                if current_square.row == 6:
-                    if board.is_square_empty(Square(current_square.row - 2, current_square.col)):
-                        next_square_two_step = Square.at(current_square.row - 2, current_square.col)
-                        return [next_square, next_square_two_step]
-                    return[next_square]
-                return [next_square]
-            return []
+            if current_square.row != 0:
+                if board.is_square_empty(Square(current_square.row - 1, current_square.col)):
+                    next_square = Square.at(current_square.row - 1, current_square.col)
+                    if current_square.row == 6:
+                        if board.is_square_empty(Square(current_square.row - 2, current_square.col)):
+                            next_square_two_step = Square.at(current_square.row - 2, current_square.col)
+                            return [next_square, next_square_two_step]
+                        return[next_square]
+                    return [next_square]
+                return []
+            return[]
 
 
 class Knight(Piece):
