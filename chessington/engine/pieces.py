0d75ca73211ac60546b164b30cc63a5b90b1072f
GOODING Andrew <agooding@altranuk.local>
2019-09-19 11:59:25 +0100

    Refactored pawn behaviour, all tests passed

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -38,68 +38,56 @@ class Pawn(Piece):
     def get_available_moves(self, board):
 
         current_square = board.find_piece(self)
+
         row_direction = 0
         start_row = 0
+        valid_moves = []
+        move_vectors = []
+
         if self.player == Player.WHITE:
             row_direction = 1
             start_row = 1
+            move_vectors = [[1, 1], [1, -1]]
+
         elif self.player == Player.BLACK:
             row_direction = -1
             start_row = 6
+            move_vectors = [[-1, 1], [-1, -1]]
 
-        valid_moves = self.check_move_exists(board, row_direction, start_row, current_square)
+        valid_moves = self.move_one_step(board, row_direction, current_square, valid_moves)
 
-        return valid_moves
+        valid_moves = self.move_two_steps(board, row_direction, current_square, start_row, valid_moves)
 
-    def check_move_exists(self, board, row_direction, start_row, current_square):
-        valid_moves = []
-        move = self.move_one_step(board, row_direction, current_square)
-        if move:
-            valid_moves.append(move)
-        move = self.move_two_steps(board, row_direction, current_square, start_row)
-        if move:
-            valid_moves.append(move)
-        move = self.capture_left(board, row_direction, current_square)
-        if move:
-            valid_moves.append(move)
-        move = self.capture_right(board, row_direction, current_square)
-        if move:
-            valid_moves.append(move)
+        if len(move_vectors) == 2:
+            for vector in range(0, len(move_vectors)):
+                valid_moves = self.capture(board, current_square, valid_moves, move_vectors[vector][0],
+                                           move_vectors[vector][1])
+
         return valid_moves
 
     @staticmethod
-    def move_one_step(board, direction, current_square):
+    def move_one_step(board, direction, current_square, valid_moves):
         next_square = Square.at(current_square.row + direction, current_square.col)
         if board.does_square_exist(next_square) and board.is_square_empty(next_square):
-            return next_square
-        return
+            valid_moves.append(next_square)
+        return valid_moves
 
     @staticmethod
-    def move_two_steps(board, direction, current_square, start_row):
+    def move_two_steps(board, direction, current_square, start_row, valid_moves):
         inter_square = Square.at(current_square.row + direction, current_square.col)
         next_square = Square.at(inter_square.row + direction, inter_square.col)
         if current_square.row == start_row and board.is_square_empty(next_square) \
                 and board.is_square_empty(inter_square):
-            return next_square
-        return
+            valid_moves.append(next_square)
+        return valid_moves
 
-    def capture_left(self, board, direction, current_square):
-        col_direction = -1
-        next_square = Square.at(current_square.row + direction, current_square.col + col_direction)
+    def capture(self, board, current_square, valid_moves, row_direction, col_direction):
+        next_square = Square.at(current_square.row + row_direction, current_square.col + col_direction)
         if board.does_square_exist(next_square) and not board.is_square_empty(next_square):
             piece = board.get_piece(next_square)
             if piece.player != self.player:
-                return next_square
-        return
-
-    def capture_right(self, board, direction, current_square):
-        col_direction = 1
-        next_square = Square.at(current_square.row + direction, current_square.col + col_direction)
-        if board.does_square_exist(next_square) and not board.is_square_empty(next_square):
-            piece = board.get_piece(next_square)
-            if piece.player != self.player:
-                return next_square
-        return
+                valid_moves.append(next_square)
+        return valid_moves
 
 
 class Knight(Piece):
